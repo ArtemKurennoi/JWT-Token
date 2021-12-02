c012dec3a9fdbcc433289a7906ea9a8f283213e5
ArtemKurennoi <work.artemkurennoi@gmail.com>
2021-12-02 13:39:17 +0300

    init

---
+++ JWT-Token/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using JWT_Token.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT_Token.Controllers
+{
+    public class AccountController : Controller
+    {
+        // тестовые данные для входа
+        private List<User> _users = new List<User>
+        {
+            new User {Login="admin@gmail.com", Password="12345", Role = "admin" },
+            new User { Login="user@gmail.com", Password="55555", Role = "user" }
+        };
+
+        //обработка запроса, принимает логин и пароль
+        [HttpPost("/token")]
+        public IActionResult Token(string username, string password)
+        {
+            //получить идентификатор пользователя
+            var identity = GetIdentity(username, password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Не правильный Логин или Email" });
+            }
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOpt.ISSUER,
+                audience: AuthOpt.AUDIENCE,
+                notBefore: now,
+
+                claims: identity.Claims,
+                expires: now.Add(TimeSpan.FromMinutes(AuthOpt.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOpt.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            //токен
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            //ответ
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+            return Json(response);
+        }//Token
+
+        //ищем пользователя в нашем списке разрешенных пользователей
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            User person = _users.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (person != null)
+            {
+                //для авторизации
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
+                };
+                //идентификатор пользователя
+                ClaimsIdentity claimsIdentity =
+                    new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                        ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
+        }//GetIdentity
+    }
+}
---
+++ JWT-Token/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using JWT_Token.Models;
+
+namespace JWT_Token
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    //для тестового проекта
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // укзывает, будет ли валидироваться издатель при валидации токена
+                        ValidateIssuer = true,
+                        // строка, представляющая издателя
+                        ValidIssuer = AuthOpt.ISSUER,
+
+                        // будет ли валидироваться потребитель токена
+                        ValidateAudience = true,
+                        // установка потребителя токена
+                        ValidAudience = AuthOpt.AUDIENCE,
+                        // будет ли валидироваться время существования
+                        ValidateLifetime = true,
+
+                        // установка ключа безопасности
+                        IssuerSigningKey = AuthOpt.GetSymmetricSecurityKey(),
+                        // валидация ключа безопасности
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
+            services.AddControllersWithViews();
+        }//ConfigureServices
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
